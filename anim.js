d70b0ef965106d5e1ce216ce9ad965a037110dd9
Mitis AluFe <81785386+mitis-alufe@users.noreply.github.com>
2025-11-23 17:17:33 +0000

    Added page exclusivity
    Now the js code only works on page 2

--- anim.js
+++ anim.js
@@ -1,15 +1,19 @@
-const fallImg= document.getElementById("fallingImage");
-const blkgrnd= document.getElementById('blackground');
-const main= document.getElementById('main');
-const links= document.getElementById('links');
+const page= document.getElementsByTagName('html')
 
-function loaded() {
-    fallImg.classList.add('fall')
-    blkgrnd.classList.add('blackin')
-    for(let i=0; i<main.children.length; i++){
-        main.children[i].classList.add('goUp')
+if(page[0].classList.contains('p2')){
+    const fallImg= document.getElementById("fallingImage");
+    const blkgrnd= document.getElementById('blackground');
+    const main= document.getElementById('main');
+    const links= document.getElementById('links');
+    
+    function loaded() {
+        fallImg.classList.add('fall')
+        blkgrnd.classList.add('blackin')
+        for(let i=0; i<main.children.length; i++){
+            main.children[i].classList.add('goUp')
+        }
+        main.classList.add('mainGone')
+        links.firstElementChild.firstElementChild.classList.add('goDown')
     }
-    main.classList.add('mainGone')
-    links.firstElementChild.firstElementChild.classList.add('goDown')
+    fallImg.addEventListener('load', loaded)    
 }
-fallImg.addEventListener('load', loaded)
