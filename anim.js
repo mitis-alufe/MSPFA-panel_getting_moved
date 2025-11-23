3d522092ee0856352bac961f62be4a75a2f7000d
Mitis AluFe <81785386+mitis-alufe@users.noreply.github.com>
2025-11-23 16:21:58 +0000

    Added short js for animation
    Now the animation only plays once the gif image is fully loaded.

---
+++ anim.js
@@ -0,0 +1,15 @@
+const fallImg= document.getElementById("fallingImage");
+const blkgrnd= document.getElementById('blackground');
+const main= document.getElementById('main');
+const links= document.getElementById('links');
+
+function loaded() {
+    fallImg.classList.add('fall')
+    blkgrnd.classList.add('blackin')
+    for(let i=0; i<main.children.length; i++){
+        main.children[i].classList.add('goUp')
+    }
+    main.classList.add('mainGone')
+    links.firstElementChild.firstElementChild.classList.add('goDown')
+}
+fallImg.addEventListener('load', loaded)
